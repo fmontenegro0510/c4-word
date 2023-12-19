436587d57758dad192933188bef960ad5c143847
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-12-19 10:44:33 -0300

    added routes and controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('form', 'FormController::index');
+$routes->post('form/generarDocumento', 'FormController::generarDocumento');
